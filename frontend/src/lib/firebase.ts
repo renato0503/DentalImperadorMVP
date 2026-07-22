e9d3eb7755c6b7eabf3aded5774ac2e0c1cafef5
renato0503 <renato0503@users.noreply.github.com>
2026-07-22 03:18:47 +0000

    deploy: 8cad32bfcdfdbf172eb91e3bb6465ec9059ca147

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCw7NIUw8WiqF1HE1aTk4Ru_CLUOloK0cU",
-  authDomain: "dentalimperador-d2529.firebaseapp.com",
-  projectId: "dentalimperador-d2529",
-  storageBucket: "dentalimperador-d2529.firebasestorage.app",
-  messagingSenderId: "330816807481",
-  appId: "1:330816807481:web:f3b98cbe9a24aaac9763e8",
-  measurementId: "G-H2DDLM3SKN",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
